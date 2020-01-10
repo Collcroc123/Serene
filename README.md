100532cfd564788475b158290c375d1966e08a3c
Collin Gale <Collin.g086@gmail.com>
2020-01-10 12:54:55 -0700

    First Commit
    Project Files

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 Serene
+A game?
---
+++ Relaxing Game/Assets/Scripts/Flashlight.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flashlight : MonoBehaviour
+{
+    public bool on = false;
+ 
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.F))
+        {
+            on = !on;
+        }
+        if(on)
+        {
+            GetComponent<Light>().enabled = true;
+        }
+        else if(!on)
+        {
+            GetComponent<Light>().enabled = false;
+        }
+    }
+}
